4ce09f413e3b4018c7eb6bbea02e1f4fe559f078
favour-the-dev <favourto91@gmail.com>
2025-11-17 10:18:36 +0100

    skills iteration v2

--- src/providers/povider.tsx
+++ src/providers/povider.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "next-themes";
 
 function AppProvider({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       {children}
     </ThemeProvider>
   );
